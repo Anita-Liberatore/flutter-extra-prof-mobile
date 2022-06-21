e09fa86adde78bf553e0af8dd3b65c02b56b48f7
Anita Liberatore <anitaliberatore@hotmail.com>
2022-06-21 17:35:32 +0200

    finish dashboard

--- lib/page/dashboard_page.dart
+++ lib/page/dashboard_page.dart
@@ -78,6 +78,7 @@ class _IndexPage2State extends State<HomePageBooked> {
   List repetitions = [];
   List bookedRepetions = [];
   bool isLoading = false;
+  var items = [];
 
   _IndexPage2State(this.loginName);
 
@@ -107,7 +108,7 @@ class _IndexPage2State extends State<HomePageBooked> {
       'connection': 'keep-alive',
       'Accept-Encoding' : 'gzip, deflate, br'});
     if(response.statusCode == 200){
-      var items = jsonDecode(response.body);
+       items = jsonDecode(response.body);
       setState(() {
         repetitions = items;
         bookedRepetions = repetitions.where((val) => val["status"] == "P").toList();
@@ -115,6 +116,54 @@ class _IndexPage2State extends State<HomePageBooked> {
     }else{
       repetitions = [];
     }
+  }
+
+  void _removeItem(int id) async {
+      repetitions = items;
+      //bookedRepetions.removeWhere((element) => element['id'] == id);
+      var url = "http://10.0.2.2:8080/backend-unito-extraprof/repetitions-delete?";
+
+      Map<String, String> qParams = {
+        'id': id.toString()
+      };
+
+      Uri uri = Uri.parse(url);
+      final finalUri = uri.replace(queryParameters: qParams);
+      var response = await http.post(
+        finalUri, headers: {'Content-Type': 'application/json; charset=UTF-8',
+        'Accept': "*/*",
+        'connection': 'keep-alive',
+        'Accept-Encoding': 'gzip, deflate, br'},
+
+        body: jsonEncode(<String, String>{
+          'status': "D",
+        }),
+      );
+      this.fetchRepetitionsByLoginName(loginName);
+  }
+
+  void _doneItem(int id) async {
+    repetitions = items;
+    //bookedRepetions.removeWhere((element) => element['id'] == id);
+    var url = "http://10.0.2.2:8080/backend-unito-extraprof/repetitions-delete?";
+
+    Map<String, String> qParams = {
+      'id': id.toString()
+    };
+
+    Uri uri = Uri.parse(url);
+    final finalUri = uri.replace(queryParameters: qParams);
+    var response = await http.post(
+      finalUri, headers: {'Content-Type': 'application/json; charset=UTF-8',
+      'Accept': "*/*",
+      'connection': 'keep-alive',
+      'Accept-Encoding': 'gzip, deflate, br'},
+
+      body: jsonEncode(<String, String>{
+        'status': "E",
+      }),
+    );
+    this.fetchRepetitionsByLoginName(loginName);
   }
 
   @override
@@ -134,36 +183,60 @@ class _IndexPage2State extends State<HomePageBooked> {
   Widget getCard(item){
     var courseName = item['courseName'];
     var hour = item['hour'];
-    return Card(
-      elevation: 1.5,
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: ListTile(
-          title: Row(
-            children: <Widget>[
-              SizedBox(width: 20,),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  new GestureDetector(
-                    onTap: () {
+    var id = item['id'];
+    var nameProfessor = item['nameProfessor'];
+    var surnameProfessor = item['surnameProfessor'];
+    var day = item['day'] == 'L' ? 'Lunedì' : item['day'] == 'M' ? 'Martedì' : item['day'] == 'ME' ? 'Mercoledì' : item['day'] == 'G' ? 'Giovedì' : item['day'] == 'V' ? 'Venerdì' : '' ;
 
-                    },
-                    child: Text(courseName),
-                  )
-                ],
+
+    return Card(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(15.0))),
+      elevation: 2,
+      margin: EdgeInsets.all(12.0),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+          child: ExpansionTile(
+              controlAffinity: ListTileControlAffinity.leading,
+              childrenPadding:
+              const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+              expandedCrossAxisAlignment: CrossAxisAlignment.start,
+              maintainState: true,
+              title: Text(courseName + " - " + "Orario lezione: "+hour),
+              // contents
+              children: [
+              Text("Nome docente: " +nameProfessor + " " +surnameProfessor), Text("Giorno lezione: " +day),
+              // This button is used to remove this item
+              TextButton.icon(
+              onPressed: () => _removeItem(id),
+              icon: const Icon(Icons.delete),
+              label: const Text(
+              'Disdici',
+
               ),
-              Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text("Orario lezione: " +hour),
-              )
-            ],
-          ),
-        ),
+
+              style: TextButton.styleFrom(primary: Colors.red),
+              ),
+                TextButton.icon(
+                  onPressed: () => _doneItem(id),
+                  icon: const Icon(Icons.done),
+                  label: const Text(
+                    'Effettuata',
+
+                  ),
+
+                  style: TextButton.styleFrom(primary: Colors.green),
+                )
+          ],
+      )
       ),
     );
   }
 }
+
+
+
+
 
 class HomePageDeleted extends StatefulWidget {
   String loginName;
@@ -231,28 +304,32 @@ class _IndexPage3State extends State<HomePageDeleted> {
   Widget getCard(item){
     var courseName = item['courseName'];
     var hour = item['hour'];
-    return Card(
-      elevation: 1.5,
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: ListTile(
-          title: Row(
-            children: <Widget>[
-              SizedBox(width: 20,),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  new GestureDetector(
-                    onTap: () {
+    var nameProfessor = item['nameProfessor'];
+    var surnameProfessor = item['surnameProfessor'];
+    var day = item['day'] == 'L' ? 'Lunedì' : item['day'] == 'M' ? 'Martedì' : item['day'] == 'ME' ? 'Mercoledì' : item['day'] == 'G' ? 'Giovedì' : item['day'] == 'V' ? 'Venerdì' : '' ;
 
-                    },
-                    child: Text(courseName),
-                  )
-                ],
-              )
+    return Card(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(15.0))),
+      elevation: 2,
+      margin: EdgeInsets.all(12.0),
+      child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ExpansionTile(
+            controlAffinity: ListTileControlAffinity.leading,
+            childrenPadding:
+            const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            expandedCrossAxisAlignment: CrossAxisAlignment.start,
+            maintainState: true,
+            title: Text(courseName + " - " + "Orario lezione: "+hour),
+            // contents
+            children: [
+              Text("Nome docente: " +nameProfessor + " " +surnameProfessor),
+              Text("Orario lezione : " +day),
+              // This button is used to remove this item
+
             ],
-          ),
-        ),
+          )
       ),
     );
   }
@@ -324,28 +401,32 @@ class _IndexPage4State extends State<HomePageDone> {
   Widget getCard(item){
     var courseName = item['courseName'];
     var hour = item['hour'];
-    return Card(
-      elevation: 1.5,
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: ListTile(
-          title: Row(
-            children: <Widget>[
-              SizedBox(width: 20,),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  new GestureDetector(
-                    onTap: () {
+    var nameProfessor = item['nameProfessor'];
+    var surnameProfessor = item['surnameProfessor'];
+    var day = item['day'] == 'L' ? 'Lunedì' : item['day'] == 'M' ? 'Martedì' : item['day'] == 'ME' ? 'Mercoledì' : item['day'] == 'G' ? 'Giovedì' : item['day'] == 'V' ? 'Venerdì' : '' ;
 
-                    },
-                    child: Text(courseName),
-                  )
-                ],
-              )
+    return Card(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(15.0))),
+      elevation: 2,
+      margin: EdgeInsets.all(12.0),
+      child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ExpansionTile(
+            controlAffinity: ListTileControlAffinity.leading,
+            childrenPadding:
+            const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            expandedCrossAxisAlignment: CrossAxisAlignment.start,
+            maintainState: true,
+            title: Text(courseName + " - " + "Orario lezione: "+hour),
+            // contents
+            children: [
+              Text("Nome docente: " +nameProfessor + " " +surnameProfessor),
+              Text("Orario lezione: " +day),
+              // This button is used to remove this item
+
             ],
-          ),
-        ),
+          )
       ),
     );
   }
